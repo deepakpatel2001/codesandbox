2b32b3a9ce192bc3277790837a353d63e35c4e0a
Deepak Patel <115418592+deepakpatel2001@users.noreply.github.com>
2023-02-05 10:44:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h3>Hello World How are you</h3>
+      <h1>Hello CodeSandbox</h1>
+      <h4>Surya Singh hai ye</h4>
+      <h2>Start editing to see some magic happen!</h2>
+      <p>Hello checking the new content</p>
+    </div>
+  );
+};
+
+export default App;
